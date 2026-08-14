efe4e7abf60fa71bd401ce88292f5e6c48e15d8d
DriveMate AI <ghadir.baraty@gmail.com>
2026-08-14 13:54:59 +0330

    Fix personal route activity import

--- app/src/main/java/ai/drivemate/PersonalRouteActivity.java
+++ app/src/main/java/ai/drivemate/PersonalRouteActivity.java
@@ -2,8 +2,8 @@ package ai.drivemate;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
